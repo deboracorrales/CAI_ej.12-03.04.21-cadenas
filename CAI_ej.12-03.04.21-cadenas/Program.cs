dafa2135a0bfc72a316b602d58c73cdc581de649
deboracorrales <deboramailen88@gmail.com>
2021-04-11 03:49:51 -0300

    Agregar archivos de proyecto.

---
+++ CAI_ej.12-03.04.21-cadenas/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CAI_ej._12_03._04._21_cadenas
+//12. Solicite el ingreso de dos frases y determine si son iguales.	
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string frase1;
+            string frase2;
+
+            Console.WriteLine("Ingrese frase 1");
+            frase1 = Console.ReadLine();
+            Console.WriteLine("Ingrese frase 2");
+            frase2 = Console.ReadLine();
+
+            Console.WriteLine(frase1 == frase2);
+
+            Console.ReadKey();
+        }
+    }
+}
